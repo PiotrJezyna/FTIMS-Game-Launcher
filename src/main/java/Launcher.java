35e1802d6e60ff68ee9c944e992acb2fb0d9aea5
Piotr <jezyna.p@gmail.com>
2019-11-16 23:44:17 +0100

    Simplest UI

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,24 +1,23 @@
-package launcher;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Launcher extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-
-        Parent root = FXMLLoader.load(getClass().getResource("Launcher.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("Launcher.fxml"));
+        primaryStage.setTitle("FTIMS Game Launcher");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
