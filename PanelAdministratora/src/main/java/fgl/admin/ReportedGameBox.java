ff524c69e463efa3f1d91c56481722f7f16d8bca
Jarosław Suchiński <ven.cjin@gmail.com>
2020-01-19 23:44:13 +0100

    checkstyle fixes

--- PanelAdministratora/src/main/java/fgl/admin/ReportedGameBox.java
+++ PanelAdministratora/src/main/java/fgl/admin/ReportedGameBox.java
@@ -9,44 +9,38 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 
 public class ReportedGameBox extends HBox {
-  Game game;
-  Label label = new Label();
-  Button discard;
-  Button delete;
-  Button show;
-
   ReportedGameBox( Game game ) {
     super();
 
-    this.game = game;
+    Label label = new Label();
     label.setText( game.getTitle() );
     label.setMaxWidth( Double.MAX_VALUE );
     HBox.setHgrow( label, Priority.ALWAYS );
 
-    discard = new Button( "Discard" );
-    discard.setOnAction(new EventHandler<ActionEvent>() {
+    Button discard = new Button( "Discard" );
+    discard.setOnAction( new EventHandler<ActionEvent>() {
       @Override
       public void handle( ActionEvent event ) {
         ModerationPanel.discardReport( game );
       }
-    });
+    } );
 
-    delete = new Button( "Delete" );
-    delete.setOnAction(new EventHandler<ActionEvent>() {
+    Button delete = new Button( "Delete" );
+    delete.setOnAction( new EventHandler<ActionEvent>() {
       @Override
       public void handle( ActionEvent event ) {
         ModerationPanel.deleteGame( game );
       }
-    });
+    } );
 
-    show = new Button( "Show game card" );
-    show.setOnAction(new EventHandler<ActionEvent>() {
+    Button show = new Button( "Show game card" );
+    show.setOnAction( new EventHandler<ActionEvent>() {
       @Override
       public void handle( ActionEvent event ) {
         // TODO Show Product Card
         // ...
       }
-    });
+    } );
 
     this.getChildren().addAll( label, discard, delete, show );
   }
